e0ef180c83d30f392d6c6dbbf81de904454386b6
Arosha Bakhtawar <aroshaamin0@gmail.com>
2026-03-01 15:20:58 +0500

    updated

--- main.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from catboost import CatBoostClassifier
-model_path = os.path.join(os.path.dirname(__file__), "CatBoostModel.cbm")
-print(model_path)
-
-model=  CatBoostClassifier()
-model.load_model(model_path)
